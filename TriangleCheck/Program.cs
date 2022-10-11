11a7e4cd216aa545cfcdc9ab752043bdebb67f4e
Alexandr Prohorov <efimpetr02@gmail.com>
2022-10-12 01:21:53 +0300

    first lab

---
+++ TriangleCheck/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+var triangleType = TriangleType.NotTriangle;
+List<string> triangleSides = new List<string>();
+
+
+
+try
+{
+    if(args.Length != 3)
+    {
+        throw new IndexOutOfRangeException();
+    }
+    for(int i = 0; i < 3; i++)
+    {
+        triangleSides.Add(args[i]);
+    }
+    float sideA = float.Parse(triangleSides[0]);
+    float sideB = float.Parse(triangleSides[1]);
+    float sideC = float.Parse(triangleSides[2]);
+    if(IsValidTriangle(sideA, sideB, sideC) == true)
+    {
+        triangleType = TriangleType.Triangle;
+    }
+    else
+    {
+        //triangleType = TriangleType.NotTriangle;
+        Console.WriteLine("NotTriangle");
+        return;
+    }
+    if(IsEquilateralTriangle(sideA, sideB, sideC) == true)
+    {
+        triangleType = TriangleType.Equilateral;
+    }
+    if(IsIsoscelesTriangle(sideA, sideB, sideC) == true)
+    {
+        triangleType = TriangleType.Isosceles;
+    }
+    Console.WriteLine(triangleType);
+}
+catch(Exception)
+{
+    Console.WriteLine("UnknownError");
+}
+
+bool IsValidTriangle(float sideA, float sideB, float sideC)
+{
+    if ((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideC + sideB > sideA))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool IsIsoscelesTriangle(float sideA, float sideB, float sideC)
+{
+    if (((sideA == sideB) && (sideA != sideC)) || ((sideA == sideC) && (sideA != sideB)) || ((sideB == sideC) && (sideB != sideA)))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool IsEquilateralTriangle(float sideA, float sideB, float sideC)
+{
+    if((sideA == sideB) && (sideA == sideC) && ((sideA > 0) && (sideB > 0) && (sideC > 0)))
+    {
+        return true;
+    }
+    return false;
+}
+
+
+enum TriangleType
+{
+    NotTriangle,
+    Triangle,
+    Equilateral,
+    Isosceles,
+}
+
+
---
+++ TriangleCheckTests/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+const string triangleTypeCheckAppName = "C:\\Users\\user\\source\\repos\\TIOPO\\lab1\\TriangleCheck\\TriangleCheck\\bin\\Debug\\net6.0\\TriangleCheck.exe";
+const string outputResultFileName = "result.txt";
+
+
+Console.WriteLine("Enter the file path");
+string? pathToInpFile = Console.ReadLine();
+if (!File.Exists(pathToInpFile))
+{
+    Console.WriteLine("Error open file.");
+    return;
+}
+using StreamReader sr = new(pathToInpFile);
+using StreamWriter sw = new(outputResultFileName);
+
+string? inputArgs = "";
+while ((inputArgs = sr.ReadLine()) != null)
+{
+    try
+    {
+        string[] testArgs = inputArgs.Split(' ');
+        if (testArgs.Length != 4) throw new ArgumentException();        
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = triangleTypeCheckAppName,
+            Arguments = $"{testArgs[0]} {testArgs[1]} {testArgs[2]}",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
+
+        using Process? proc = Process.Start(startInfo);
+        if (proc != null)
+        {
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string? result = proc.StandardOutput.ReadLine();
+
+                string outputRes = (testArgs[3] == result) ? "success" : "error";
+
+                sw.WriteLine(outputRes);
+            }
+        }
+    }
+    catch (Exception)
+    {
+        sw.WriteLine("error");
+    }
+}
+/*
+struct TestCase
+{
+    public string a;
+    public string b;
+    public string c;
+    public string result;
+}
+*/
+//C:\Users\user\source\repos\TIOPO\lab1\TriangleCheck\tests\TestSuccesCase.txt
+//C:\Users\user\source\repos\TIOPO\lab1\TriangleCheck\tests\TestErrorCase.txt
